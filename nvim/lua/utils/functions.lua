8ee3ae532bafbbb1df89fc4280255a7f5ef4c265
Ren B <127970608+Ren-B-7@users.noreply.github.com>
2024-08-24 23:34:42 +0200

    Refractor functions output

--- nvim/lua/utils/functions.lua
+++ nvim/lua/utils/functions.lua
@@ -3,9 +3,11 @@ local INFO = vim.log.levels.info
 local TRACE = vim.log.levels.trace
 local WARN = vim.log.levels.warn
 local ERROR = vim.log.levels.error
-local OFF = vim.log.levels.off
+local OFF = 0
 
-local notify = vim.notify
+local notify = function(message, log)
+	local out = log ~= OFF and vim.notify(message, log) or nil
+end
 
 local api = vim.api
 local cmd = vim.cmd
@@ -15,38 +17,37 @@ local get_opts = api.nvim_get_option_value
 
 local utils = {}
 
-utils.toggle_list_and_col = function(message)
-	message = message or INFO
-	notify("Toggle column and list", message)
-
+utils.toggle_list_and_col = function(log)
+	log = log == OFF and OFF or INFO
+	notify("Toggle column and list", log)
 	utils.toggle_list(OFF)
 	utils.toggle_colorcol(OFF)
 end
 
-utils.toggle_colorcol = function(message)
-	message = message or INFO
-	notify("Toggle colorcolumn", message)
+utils.toggle_colorcol = function(log)
+	log = log == OFF and OFF or INFO
+	notify("Toggle colorcolumn", log)
 
 	local val = (get_opts("colorcolumn", {}) == "" and "80" or "")
 	set_opts("colorcolumn", val, {})
 end
 
-utils.toggle_list = function(message)
-	message = message or INFO
-	notify("Toggle list", message)
+utils.toggle_list = function(log)
+	log = log == OFF and OFF or INFO
+	notify("Toggle list", log)
 	cmd("set list!")
 end
 
-utils.toggle_modifiable = function(message)
-	message = message or INFO
-	notify("Toggle modifiable", message)
+utils.toggle_modifiable = function(log)
+	log = log == OFF and OFF or INFO
+	notify("Toggle modifiable", log)
 
 	cmd("setlocal modifiable!")
 end
 
-utils.reload_nvim_tree = function(message)
-	message = message or INFO
-	notify("Reload Nvim Tree", message)
+utils.reload_nvim_tree = function(log)
+	log = log == OFF and OFF or INFO
+	notify("Reload Nvim Tree", log)
 
 	require("nvim-tree.api").tree.open()
 	require("nvim-tree.api").tree.reload()
