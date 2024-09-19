f1bfa92ab7074b64d9552567cca21b81ad5f7262
Ren B <127970608+Ren-B-7@users.noreply.github.com>
2024-09-19 22:21:53 +0200

    Small changes to Indent blank line
    Signed-off-by: Ren B <127970608+Ren-B-7@users.noreply.github.com>

--- nvim/lua/plugins/indent_blank_line.lua
+++ nvim/lua/plugins/indent_blank_line.lua
@@ -7,19 +7,29 @@ local options = function()
 		"RainbowGreen",
 		"RainbowViolet",
 		"RainbowCyan",
+		"SandBeige",
+		"GoldenSun",
+		"SunsetPink",
+		"OceanTeal",
+		"MountainGray",
 	}
 
 	local hooks = require("ibl.hooks")
-	-- create the highlight groups in the highlight setup hook, so they are reset
-	-- every time the colorscheme changes
+	local create_hl = vim.api.nvim_set_hl
+
 	hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-		vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-		vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-		vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-		vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-		vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-		vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-		vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+		create_hl(0, "RainbowRed", { fg = "#E06C75" })
+		create_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+		create_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+		create_hl(0, "RainbowOrange", { fg = "#D19A66" })
+		create_hl(0, "RainbowGreen", { fg = "#98C379" })
+		create_hl(0, "RainbowViolet", { fg = "#C678DD" })
+		create_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+		create_hl(0, "SandBeige", { fg = "#F4A460" })
+		create_hl(0, "GoldenSun", { fg = "#FFD700" })
+		create_hl(0, "SunsetPink", { fg = "#FCB6A4" })
+		create_hl(0, "OceanTeal", { fg = "#008080" })
+		create_hl(0, "MountainGray", { fg = "#A9A9A9" })
 	end)
 
 	return {
